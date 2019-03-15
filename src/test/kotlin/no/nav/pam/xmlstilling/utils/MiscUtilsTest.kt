e9ec12d3b3e6a38c5168c68f61fa67027c61dcb2
Øyvind Hagen <oyvind.hagen@nav.no>
2019-03-15 15:04:11 +0100

    Fikset en feil i regexp for å fjerne encoding. Var litt for grådig.

--- src/test/kotlin/no/nav/pam/xmlstilling/utils/MiscUtilsTest.kt
+++ src/test/kotlin/no/nav/pam/xmlstilling/utils/MiscUtilsTest.kt
@@ -8,7 +8,7 @@ class MiscUtilsTest {
     @Test
     fun testStripEncoding() {
         assertThat(stripEncoding("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>")).isEqualTo("<?xml version=\"1.0\"?>")
-        assertThat(stripEncoding("<?xml version=\"1.0\" EncoDING=\"isO-8859-1\"?>")).isEqualTo("<?xml version=\"1.0\"?>")
+        assertThat(stripEncoding("<?xml version=\"1.0\" EncoDING=\"isO-8859-1\"?><b a=\"c\">")).isEqualTo("<?xml version=\"1.0\"?><b a=\"c\">")
         assertThat(stripEncoding("<?xml version=\"1.0\" encoding=\"utf-8\"?>")).isEqualTo("<?xml version=\"1.0\"?>")
         assertThat(stripEncoding("<?xml version=\"1.0\" encoding=\"Windows-1252\"?>")).isEqualTo("<?xml version=\"1.0\"?>")
         assertThat(stripEncoding("\n\n<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n\n<foo>bar</foo>"))
