dbbf5dfeb77cbffd40e294e41a0090a940c846ad
Einar Bjerve <einar.bjerve@nav.no>
2019-05-27 09:01:09 +0200

    Refaktorerte stripping av encoding til extension funksjon for lesbarhet. Reduserte også synlighet når den ble flyttet

--- src/main/kotlin/no/nav/pam/xmlstilling/utils/MiscUtils.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.pam.xmlstilling.utils
-
-import java.util.regex.Pattern
-
-fun stripEncoding(s: String): String {
-    val pattern = Pattern.compile("\\sencoding=\"[^\"]+\"", Pattern.MULTILINE or Pattern.CASE_INSENSITIVE)
-    return pattern.matcher(s).replaceAll("")
-}
